e9ecb4eebdcdab10f5e707be959e89d9e01ebf26
JulioMarte <julio19955@hotmail.com>
2026-05-14 12:13:11 -0400

    Add Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
